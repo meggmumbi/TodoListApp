39ba7d962d3edc7a5478a78069dddbee4a157c13
margaretmumbi16@gmail.com <margaretmumbi16@gmail.com>
2022-03-03 17:30:58 +0300

    Add project files.

---
+++ TodoListApp/TodoListApp/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoListApp
+{   
+     public class TodoItem
+    {
+        public string TodoText { get; set; }
+        public bool complete { get; set; }
+
+        public TodoItem(string TodoText, bool Complete)
+        {
+            this.TodoText = TodoText;
+            this.complete = complete;
+
+        }
+    }
+}
---
+++ TodoListApp/TodoListApp/TodoListViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems {get;set;}
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            
+
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodInputValue { get; set; }
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+
+
+    }
+}
